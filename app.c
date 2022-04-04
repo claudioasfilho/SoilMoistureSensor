bc1494e9a01127b6b208f9f6a77b8dd145c8be3f
Claudio Filho <claudioasfilho@gmail.com>
2022-04-04 11:23:10 -0700

    first commit

---
+++ app.c
@@ -0,0 +1,364 @@
+/***************************************************************************//**
+ * @file
+ * @brief Core application logic.
+ *******************************************************************************
+ * # License
+ * <b>Copyright 2020 Silicon Laboratories Inc. www.silabs.com</b>
+ *******************************************************************************
+ *
+ * SPDX-License-Identifier: Zlib
+ *
+ * The licensor of this software is Silicon Laboratories Inc.
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ *
+ ******************************************************************************/
+#include "em_common.h"
+#include "app_assert.h"
+#include "sl_bluetooth.h"
+#include "gatt_db.h"
+#include "app.h"
+
+
+//IADC
+
+#include <stdio.h>
+#include "em_device.h"
+#include "em_chip.h"
+#include "em_cmu.h"
+#include "em_emu.h"
+#include "em_iadc.h"
+#include "em_gpio.h"
+#include "em_prs.h"
+
+// The advertising set handle allocated from Bluetooth stack.
+static uint8_t advertising_set_handle = 0xff;
+
+
+
+/*******************************************************************************
+ *******************************   DEFINES   ***********************************
+ ******************************************************************************/
+
+// Set CLK_ADC to 10MHz (this corresponds to a sample rate of 77K with OSR = 32)
+// CLK_SRC_ADC; largest division is by 4
+#define CLK_SRC_ADC_FREQ        4000
+
+// CLK_ADC; IADC_SCHEDx PRESCALE has 10 valid bits
+#define CLK_ADC_FREQ            1000
+
+// When changing GPIO port/pins above, make sure to change xBUSALLOC macro's
+// accordingly.
+#define IADC_INPUT_BUS          CDBUSALLOC
+#define IADC_INPUT_BUSALLOC     GPIO_CDBUSALLOC_CDEVEN0_ADC0
+
+#define ADC_INT 0xaaaa
+
+/*******************************************************************************
+ ***************************   GLOBAL VARIABLES   ******************************
+ ******************************************************************************/
+
+// Stores latest ADC sample and converts to volts
+static volatile IADC_Result_t sample;
+static volatile double singleResult;
+
+/**************************************************************************//**
+ * @brief  GPIO Initializer
+ *****************************************************************************/
+void initGPIO(void)
+{
+  // Enable GPIO clock branch
+  CMU_ClockEnable(cmuClock_GPIO, true);
+
+  // Configure PB1 as output, will indicate when conversions are being performed
+  GPIO_PinModeSet(gpioPortB, 1, gpioModePushPull, 0);
+}
+
+/**************************************************************************//**
+ * @brief  PRS Initializer
+ *****************************************************************************/
+void initPRS(void)
+{
+  // Enable PRS clock
+  CMU_ClockEnable(cmuClock_PRS, true);
+
+  // Connect PRS Async channel 0 to ADC single complete signal
+  PRS_SourceAsyncSignalSet(0, PRS_ASYNC_CH_CTRL_SOURCESEL_IADC0,
+                           PRS_ASYNC_CH_CTRL_SIGSEL_IADC0SINGLEDONE);
+
+  // Route PRS channel 0 to PB1 to indicate a conversion complete
+  PRS_PinOutput(0,prsTypeAsync, gpioPortB, 1);
+}
+
+/**************************************************************************//**
+ * @brief  IADC Initializer
+ *****************************************************************************/
+void initIADC(void)
+{
+  // Declare init structs
+  IADC_Init_t init = IADC_INIT_DEFAULT;
+  IADC_AllConfigs_t initAllConfigs = IADC_ALLCONFIGS_DEFAULT;
+  IADC_InitSingle_t initSingle = IADC_INITSINGLE_DEFAULT;
+  IADC_SingleInput_t initSingleInput = IADC_SINGLEINPUT_DEFAULT;
+
+  // Enable IADC clock
+  CMU_ClockEnable(cmuClock_IADC0, true);
+
+  // Reset IADC to reset configuration in case it has been modified
+  IADC_reset(IADC0);
+
+  // Configure IADC clock source for use while in EM2
+//  CMU_ClockSelectSet(cmuClock_IADCCLK, cmuSelect_FSRCO);
+
+  // Modify init structs and initialize
+  init.warmup = iadcWarmupKeepWarm;
+
+  // Set the HFSCLK prescale value here
+  init.srcClkPrescale = IADC_calcSrcClkPrescale(IADC0, CLK_SRC_ADC_FREQ, 0);
+
+  // Configuration 0 is used by both scan and single conversions by default
+  // Use unbuffered AVDD as reference
+  initAllConfigs.configs[0].reference = iadcCfgReferenceVddx;
+
+  // Divides CLK_SRC_ADC to set the CLK_ADC frequency for desired sample rate
+  initAllConfigs.configs[0].adcClkPrescale = IADC_calcAdcClkPrescale(IADC0,
+                                                                    CLK_ADC_FREQ,
+                                                                    0,
+                                                                    iadcCfgModeNormal,
+                                                                    init.srcClkPrescale);
+
+  // Set oversampling rate to 32x
+  // resolution formula res = 11 + log2(oversampling * digital averaging)
+  // in this case res = 11 + log2(32*1) = 16
+  initAllConfigs.configs[0].osrHighSpeed = iadcCfgOsrHighSpeed32x;
+
+
+  // Single initialization
+  initSingle.dataValidLevel = _IADC_SINGLEFIFOCFG_DVL_VALID1;
+
+  // Set conversions to run continuously
+  initSingle.triggerAction = iadcTriggerActionContinuous;
+
+  // Set alignment to right justified with 16 bits for data field
+  initSingle.alignment = iadcAlignRight16;
+
+  // Configure Input sources for single ended conversion
+  initSingleInput.posInput = iadcPosInputPortCPin2;
+  initSingleInput.negInput = iadcNegInputGnd;
+
+  // Initialize IADC
+  // Note oversampling and digital averaging will affect the offset correction
+  // This is taken care of in the IADC_init() function in the emlib
+  IADC_init(IADC0, &init, &initAllConfigs);
+
+  // Initialize Scan
+  IADC_initSingle(IADC0, &initSingle, &initSingleInput);
+
+  // Allocate the analog bus for ADC0 inputs
+  GPIO->IADC_INPUT_BUS |= IADC_INPUT_BUSALLOC;
+
+  // Enable interrupts on data valid level
+  IADC_enableInt(IADC0, IADC_IEN_SINGLEFIFODVL);
+
+  // Enable ADC interrupts
+  NVIC_ClearPendingIRQ(IADC_IRQn);
+  NVIC_EnableIRQ(IADC_IRQn);
+}
+
+/**************************************************************************//**
+ * @brief  ADC Handler
+ *****************************************************************************/
+void IADC_IRQHandler(void)
+{
+  // Read data from the FIFO, 16-bit result
+  sample = IADC_pullSingleFifoResult(IADC0);
+
+
+//  // For single-ended the result range is 0 to +Vref, i.e., 16 bits for the
+//  // conversion value.
+//  singleResult = sample.data * 3.3 / 0xFFFF;
+//
+//  app_log("Raw = %d, Voltage = %f \n\r", sample, (float)singleResult);
+
+  IADC_clearInt(IADC0, IADC_IF_SINGLEFIFODVL);
+
+  sl_bt_external_signal(1);
+}
+
+///**************************************************************************//**
+// * @brief  Main function
+// *****************************************************************************/
+//int test(void)
+//{
+////  CHIP_Init();
+////
+////  // Initialize GPIO
+////  initGPIO();
+////
+////  // Initialize PRS
+////  initPRS();
+////
+////  // Initialize the IADC
+////  initIADC();
+////
+////  // Start single
+////  IADC_command(IADC0, iadcCmdStartSingle);
+////
+////  while (1)
+////  {
+////    // Enter EM2 sleep, woken by IADC interrupt
+////    EMU_EnterEM2(true);
+////  }
+//}
+
+/**************************************************************************//**
+ * Application Init.
+ *****************************************************************************/
+SL_WEAK void app_init(void)
+{
+  /////////////////////////////////////////////////////////////////////////////
+  // Put your additional application init code here!                         //
+  // This is called once during start-up.                                    //
+  /////////////////////////////////////////////////////////////////////////////
+
+  // Initialize GPIO
+  initGPIO();
+
+  // Initialize PRS
+  initPRS();
+
+//  // Initialize the IADC
+  initIADC();
+//
+  app_log("ADC Initialized\n\r");
+//
+//  // Start single
+  IADC_command(IADC0, iadcCmdStartSingle);
+}
+
+/**************************************************************************//**
+ * Application Process Action.
+ *****************************************************************************/
+SL_WEAK void app_process_action(void)
+{
+  /////////////////////////////////////////////////////////////////////////////
+  // Put your additional application code here!                              //
+  // This is called infinitely.                                              //
+  // Do not call blocking functions from here!                               //
+  /////////////////////////////////////////////////////////////////////////////
+}
+
+/**************************************************************************//**
+ * Bluetooth stack event handler.
+ * This overrides the dummy weak implementation.
+ *
+ * @param[in] evt Event coming from the Bluetooth stack.
+ *****************************************************************************/
+void sl_bt_on_event(sl_bt_msg_t *evt)
+{
+  sl_status_t sc;
+  bd_addr address;
+  uint8_t address_type;
+  uint8_t system_id[8];
+
+  switch (SL_BT_MSG_ID(evt->header)) {
+    // -------------------------------
+    // This event indicates the device has started and the radio is ready.
+    // Do not call any stack command before receiving this boot event!
+    case sl_bt_evt_system_boot_id:
+
+      app_log("device boots \n\r");
+      // Extract unique ID from BT Address.
+      sc = sl_bt_system_get_identity_address(&address, &address_type);
+      app_assert_status(sc);
+
+      // Pad and reverse unique ID to get System ID.
+      system_id[0] = address.addr[5];
+      system_id[1] = address.addr[4];
+      system_id[2] = address.addr[3];
+      system_id[3] = 0xFF;
+      system_id[4] = 0xFE;
+      system_id[5] = address.addr[2];
+      system_id[6] = address.addr[1];
+      system_id[7] = address.addr[0];
+
+      sc = sl_bt_gatt_server_write_attribute_value(gattdb_system_id,
+                                                   0,
+                                                   sizeof(system_id),
+                                                   system_id);
+      app_assert_status(sc);
+
+      // Create an advertising set.
+      sc = sl_bt_advertiser_create_set(&advertising_set_handle);
+      app_assert_status(sc);
+
+      // Set advertising interval to 100ms.
+      sc = sl_bt_advertiser_set_timing(
+        advertising_set_handle,
+        160, // min. adv. interval (milliseconds * 1.6)
+        160, // max. adv. interval (milliseconds * 1.6)
+        0,   // adv. duration
+        0);  // max. num. adv. events
+      app_assert_status(sc);
+      // Start general advertising and enable connections.
+      sc = sl_bt_advertiser_start(
+        advertising_set_handle,
+        sl_bt_advertiser_general_discoverable,
+        sl_bt_advertiser_connectable_scannable);
+      app_assert_status(sc);
+      app_log("advertising \n\r");
+      break;
+
+
+    case sl_bt_evt_system_external_signal_id:
+          // Handle GPIO IRQ and do something
+    // External signal command’s parameter can be accessed using
+
+      app_log("interrupt Context \n\r");
+
+     if( evt->data.evt_system_external_signal.extsignals == 1)
+       {
+         app_log("Raw = %d, Voltage = %f \n\r", sample, (float)singleResult);
+       }
+    break;
+
+    // -------------------------------
+    // This event indicates that a new connection was opened.
+    case sl_bt_evt_connection_opened_id:
+      break;
+
+    // -------------------------------
+    // This event indicates that a connection was closed.
+    case sl_bt_evt_connection_closed_id:
+      // Restart advertising after client has disconnected.
+      sc = sl_bt_advertiser_start(
+        advertising_set_handle,
+        sl_bt_advertiser_general_discoverable,
+        sl_bt_advertiser_connectable_scannable);
+      app_assert_status(sc);
+      break;
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Add additional event handlers here as your application requires!      //
+    ///////////////////////////////////////////////////////////////////////////
+
+    // -------------------------------
+    // Default event handler.
+    default:
+      break;
+  }
+}
